ce8b6a98a759737e8a18c4b990ccb4c9a0f02374
simowardi <daoist32@gmail.com>
2024-07-10 16:58:39 +0100

    update config file

--- config.py
+++ config.py
@@ -1,13 +1,8 @@
 import os
 
 class Config:
-<<<<<<< HEAD
-    SECRET_KEY = os.environ.get('az12', '123456789AZERTYUIOP')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:zeé"ZE2323@localhost/my_flask_app')
-=======
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///shareblast.db'
->>>>>>> refs/remotes/origin/master
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 # not used for now , but can be used later
