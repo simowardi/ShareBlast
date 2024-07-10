a75d3864200906739c4958d673f1430097057573
simowardi <daoist32@gmail.com>
2024-07-10 16:36:15 +0100

    update config file

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('az12', '123456789AZERTYUIOP')
-    SQLALCHEMY_DATABASE_URI = (
-        os.environ.get('DATABASE_URL') or 'mysql://root:zeé"ZE2323@localhost/my_flask_app'
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'mysql://root:zeé"ZE2323@localhost/my_flask_app'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
